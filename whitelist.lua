5a232ab0fbf3099c34e9e922f900f75ed19e09f9
bladstaz01 <bladstaz01@gmail.com>
2025-12-04 16:00:39 +0800

    Update whitelist.lua
    updated

--- whitelist.lua
+++ whitelist.lua
@@ -1,29 +1,34 @@
 local W = {}
-
---==============WHITELIST START
 local whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/bladstaz01/git_whitelist_string/refs/heads/main/whitelist_string"))()
---==============WHITELIST END
 
-
--- Helper to get current GMT+8 timestamp
 local function current_gmt8()
-    local utc = os.time(os.date("!*t"))
-    return utc + 8 * 3600
+	local utc = os.time(os.date("!*t"))
+	return utc + 8 * 3600
 end
 
--- Verify if user is whitelisted and still has credits
-function W.verify(username)
-    local expiry = whitelist[username]
-    if not expiry then
-        return false -- not whitelisted
-    end
+local function format_gmt8(ts)
+	if not ts then return nil end
+	-- Interpret ts as absolute epoch, convert to UTC table, then add 8 hours and reformat
+	local utc_tbl = os.date("!*t", ts)
+	local ts_gmt8 = os.time(utc_tbl) + 8 * 3600
+	local t = os.date("*t", ts_gmt8)
+	return string.format("%02d/%02d/%04d %02d:%02d:%02d", t.month, t.day, t.year, t.hour, t.min, t.sec)
+end
 
-    return current_gmt8() <= expiry
+function W.verify(username)
+	local expiry = whitelist[username]
+	if not expiry then
+		return false
+	end
+	return current_gmt8() <= expiry
 end
 
 function W.getExpiry(username)
-    local expiry = whitelist[username]
-    return expiry
+	local expiry = whitelist[username]
+	if not expiry then
+		return nil
+	end
+	return format_gmt8(expiry)
 end
 
 return W
